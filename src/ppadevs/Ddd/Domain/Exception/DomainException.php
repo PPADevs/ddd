d8a6e2f3ff2c85fdeee278abf3cdad4c391d5aa2
ngaspari <ngaspari@gmail.com>
2022-02-09 10:29:22 +0100

    HF
    parameters be always []
    ...because vsprintf expects array

--- src/ppadevs/Ddd/Domain/Exception/DomainException.php
+++ src/ppadevs/Ddd/Domain/Exception/DomainException.php
@@ -3,16 +3,25 @@ namespace ppadevs\Ddd\Domain\Exception;
 
 class DomainException extends \Exception
 {
-	private $parameters = array();
+	/** @var array */
+	private $parameters = [];
 	
-	public function __construct($message, $parameters)
+	public function __construct($message, ?array $parameters = [])
 	{
-		$this->parameters = $parameters;
+		$this->setParameters( $parameters );
 		parent::__construct($message);
 	}
 	
+	/**
+	 * @return array
+	 */
 	public function getParameters()
 	{
 		return $this->parameters;
+	}
+	
+	public function setParameters( $parameters )
+	{
+		$this->parameters = !empty($parameters) ? $parameters : [];
 	}
 }
